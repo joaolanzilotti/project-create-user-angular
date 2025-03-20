acda38242e57d18bb11c88c24d50b25485276ca0
JoaoLanzilotti <algamerjoao1@hotmail.com>
2025-03-19 22:12:09 -0300

    Signals Implemented in the userComponent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal, Signal} from '@angular/core';
 
 import { DUMMY_USERS} from "../dummy-users";
 
@@ -12,5 +12,24 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // I have two options to update the states in angular,
+  // 1 option is automatic , 2 option is using signals
+  // 2 option
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+ imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  // 1 option
+  // selectedUser = DUMMY_USERS[randomIndex];
+
+  // get imagePath(){
+  //   return 'assets/users/' + this.selectedUser().avatar ;
+  // }
+
+  onSelectUser(){
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // 2 option
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // 1 option
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+
 }
