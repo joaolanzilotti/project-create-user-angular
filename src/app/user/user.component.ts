6d610a630af1001a549693dc9991e834fd1c7e86
joaolanzilotti <algamerjoao1@hotmail.com>
2025-03-22 20:05:59 -0300

    Made a new component tasks and did after to click in the user, select the user and show his name.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import {Component, computed, signal, Signal} from '@angular/core';
-
-import { DUMMY_USERS} from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, Input, Output, EventEmitter, output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,24 +8,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // I have two options to update the states in angular,
-  // 1 option is automatic , 2 option is using signals
-  // 2 option
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
- imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-  // 1 option
-  // selectedUser = DUMMY_USERS[randomIndex];
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
 
-  // get imagePath(){
-  //   return 'assets/users/' + this.selectedUser().avatar ;
-  // }
-
-  onSelectUser(){
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // 2 option
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    // 1 option
-    // this.selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
 
+  onSelectUser(){
+    // That form I emit the value to the app.component.html
+    this.select.emit(this.id);
+  }
 }
