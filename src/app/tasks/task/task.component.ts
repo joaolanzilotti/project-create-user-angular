71f1e9f2c45e3debfa5f55dd9324c2ada8acdc77
joaolanzilotti <algamerjoao1@hotmail.com>
2025-03-27 23:38:32 -0300

    add a new task component and a button to close de modal new task

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {type Task} from "./task.model";
 
 @Component({
   selector: 'app-task',
@@ -9,4 +10,10 @@ import {Component} from '@angular/core';
 })
 export class TaskComponent {
 
+  @Input({required: true}) task!: Task;
+  @Output() complete = new EventEmitter<string>();
+
+  onCompleteTask(){
+    this.complete.emit(this.task.id);
+  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {TaskComponent} from "./task/task.component";
+import {NewTaskComponent} from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [
-    TaskComponent
+    TaskComponent,
+    NewTaskComponent
   ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
@@ -13,6 +15,7 @@ import {TaskComponent} from "./task/task.component";
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
+  isAddingTask = false;
   tasks = [
     {
       id: 't1',
@@ -43,4 +46,15 @@ export class TasksComponent {
     return this.tasks.filter(task => task.userId === this.userId);
   }
 
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
 }
